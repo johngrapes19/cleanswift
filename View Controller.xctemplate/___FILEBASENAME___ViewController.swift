abb945616b079dbd61b05d773587b295dcee690c
johngrapes19 <119903804+johngrapes19@users.noreply.github.com>
2023-06-19 18:45:54 +0300

    Add files via upload

---
+++ View Controller.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___DisplayLogic: AnyObject {
+    func displaySomething(viewModel: ___VARIABLE_sceneName___.Something.ViewModel)
+}
+
+class ___VARIABLE_sceneName___ViewController: UIViewController, ___VARIABLE_sceneName___DisplayLogic {
+    var interactor: ___VARIABLE_sceneName___BusinessLogic?
+    var router: (NSObjectProtocol & ___VARIABLE_sceneName___RoutingLogic & ___VARIABLE_sceneName___DataPassing)?
+
+    // MARK: - Object lifecycle
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setupScene()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupScene()
+    }
+
+    // MARK: - Setup
+
+    private func setupScene() {
+        let viewController = self
+        let interactor = ___VARIABLE_sceneName___Interactor()
+        let presenter = ___VARIABLE_sceneName___Presenter()
+        let router = ___VARIABLE_sceneName___Router()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+
+    // MARK: - Routing
+
+    override func prepare(for segue: UIStoryboardSegue, sender _: Any?) {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+
+    // MARK: - View lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        doSomething()
+    }
+
+    // MARK: - Display Logic
+
+    func displaySomething(viewModel _: ___VARIABLE_sceneName___.Something.ViewModel) {
+        // nameTextField.text = viewModel.name
+    }
+
+    // MARK: - Private
+
+    func doSomething() {
+        let request = ___VARIABLE_sceneName___.Something.Request()
+        interactor?.doSomething(request: request)
+    }
+}

7ca9f70beade0c75148b100a56ad13bbd8177fae
johngrapes19 <119903804+johngrapes19@users.noreply.github.com>
2023-06-19 18:46:31 +0300

    Add files via upload

---
+++ Worker.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,16 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import UIKit
+
+protocol ___VARIABLE_sceneName___WorkerLogic {
+    func doSomeWork()
+}
+
+class ___VARIABLE_sceneName___Worker: ___VARIABLE_sceneName___WorkerLogic {
+    func doSomeWork() {}
+}
